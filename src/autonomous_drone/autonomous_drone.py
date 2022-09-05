0311424947a4b71891f1b82f4da3411351874c65
FredOliveira <fred.carvalho.oliveira97@gmail.com>
2022-09-05 16:51:27 +0100

    integrated pose detection in drone code

--- src/autonomous_drone/autonomous_drone.py
+++ src/autonomous_drone/autonomous_drone.py
@@ -1,7 +1,7 @@
 from djitellopy import Tello
 from src.pose_detection.body import Body
 from src.autonomous_drone.non_blocking_wait import NonBlockingWait
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 import cv2
 
 class AutonomousDrone:
@@ -14,7 +14,7 @@ class AutonomousDrone:
 
         self.frame_read = self.tello.get_frame_read()
 
-        self.pose_model = Body('../res/model/body_pose_model.pth')
+        self.pose_model = Body('./res/model/body_pose_model.pth')
 
         self.wait = NonBlockingWait()
         self.SHOULD_RUN = True
@@ -29,8 +29,27 @@ class AutonomousDrone:
     def get_frame(self):
         return self.frame_read.frame
 
-    def predict_pose(self, img) -> List[Tuple]:
-        candidate, subset = self.pose_model(img)
+    def __resize_frame(self, frame):
+        scale_percent = 20  # percent of original size
+        width = int(frame.shape[1] * scale_percent / 100)
+        height = int(frame.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
+    def predict_pose(self, frame) -> List[Optional[Tuple[int, int]]]:
+        keypoints_to_track = [0]
+        keypoints = [None for i in range(len(keypoints_to_track))]
+        candidate, subset = self.pose_model(frame)
+
+        if len(subset) > 0:
+            point_indexes = subset[0]  # Track only one person
+
+            for idx, keypoint in enumerate(keypoints_to_track):
+                point_idx = int(point_indexes[keypoint])
+                if point_idx != -1:
+                    x, y = candidate[point_idx][0:2]
+                    keypoints[idx] = (int(x), int(y))
+        return keypoints
 
     def calc_speeds(self, keypoints: List[Tuple]) -> List[int]:
         pass
@@ -39,23 +58,32 @@ class AutonomousDrone:
         pass
 
     def run(self, debug=False):
-        self.takeoff()
+        # self.takeoff()
 
-        if self.wait.has_time_passed():
-            while self.SHOULD_RUN:
-                frame = self.get_frame()
-                keypoints = self.predict_pose(img=frame)
-                speeds = self.calc_speeds(keypoints=keypoints)
-                self.update_motor_speeds(speeds=speeds)
+        while self.SHOULD_RUN:
+            frame = self.get_frame()
+            frame = self.__resize_frame(frame)
 
-                cv2.imshow('Drone View', frame)
-                k = cv2.waitKey(1)
-                if k == ord('q'):
-                    break
-                elif k == ord('s'):
-                    print('>>> Start take off')
-            # Stop the drone
-            self.tello.end()
+            keypoints = self.predict_pose(frame=frame)
+
+            if debug:
+                for point in keypoints:
+                    if point is not None:
+                        cv2.circle(frame, (point[0], point[1]), 4, (253, 1, 36), thickness=-1)
+
+            speeds = self.calc_speeds(keypoints=keypoints)
+            # self.update_motor_speeds(speeds=speeds)
+
+            cv2.imshow('Drone View', frame)
+            k = cv2.waitKey(1)
+            if k == ord('q'):
+                break
+            elif k == ord('s'):
+                print('>>> Taking off')
+        # Stop the drone
+        print('>>> Turning off drone')
+        cv2.destroyAllWindows()
+        self.tello.end()
 
     def stop(self):
         self.SHOULD_RUN = False
--- src/demo_camera.py
+++ src/demo_camera.py
@@ -4,6 +4,21 @@ import torch
 from djitellopy import Tello
 from src.pose_detection.body import Body
 
+
+def predict_pose(model, img):
+    keypoints_to_track = [0]
+    keypoints = [None for i in range(len(keypoints_to_track))]
+    candidate, subset = model(img)
+
+    point_indexes = subset[0]  # Track only one person
+
+    for idx, keypoint in enumerate(keypoints_to_track):
+        point_idx = int(point_indexes[keypoint])
+        if point_idx != -1:
+            x, y = candidate[point_idx][0:2]
+            keypoints[idx] = (int(x), int(y))
+    return keypoints
+
 body_estimation = Body('../res/model/body_pose_model.pth')
 
 print(f"Torch device: {torch.cuda.get_device_name()}")
@@ -13,27 +28,40 @@ cap.set(3, 160)
 cap.set(4, 120)
 while True:
     ret, oriImg = cap.read()
-    candidate, subset = body_estimation(oriImg)
+    # candidate, subset = body_estimation(oriImg)
     canvas = copy.deepcopy(oriImg)
-    # NOSE 0
-    # CHEST 1
+    # # NOSE 0
+    # # CHEST 1
+    #
+    # center_x = int(oriImg.shape[1]/2)
+    # center_y = int(oriImg.shape[0]/2)
+    #
+    # print(subset.shape)
+    # print(subset)
+    # print(subset[0][0])
 
-    center_x = int(oriImg.shape[1]/2)
-    center_y = int(oriImg.shape[0]/2)
+    points = predict_pose(body_estimation, oriImg)
+    print(points)
 
-    for i in [0]:
-        for n in range(len(subset)):
-        # for n in [0]:
-            index = int(subset[n][i])
-            # POINT NOT FOUND
-            if index == -1:
-                continue
-            x, y = candidate[index][0:2]
-            cv2.circle(canvas, (int(x), int(y)), 4, (253, 1, 36), thickness=-1)
-            cv2.circle(canvas, (int(center_x), int(center_y)), 4, (0, 0, 255), thickness=-1)
+    for point in points:
+        if point is not None:
+            cv2.circle(canvas, (point[0], point[1]), 4, (253, 1, 36), thickness=-1)
 
-            diff_x = (x - center_x)/(oriImg.shape[1]/2)
-            diff_y = (center_y - y)/(oriImg.shape[0]/2)
+    # cv2.circle(canvas, (int(center_x), int(center_y)), 4, (0, 0, 255), thickness=-1)
+
+    # for i in [0]:
+    #     for n in range(len(subset)):
+    #     # for n in [0]:
+    #         index = int(subset[n][i])
+    #         # POINT NOT FOUND
+    #         if index == -1:
+    #             continue
+    #         x, y = candidate[index][0:2]
+    #         cv2.circle(canvas, (int(x), int(y)), 4, (253, 1, 36), thickness=-1)
+    #         cv2.circle(canvas, (int(center_x), int(center_y)), 4, (0, 0, 255), thickness=-1)
+    #
+    #         diff_x = (x - center_x)/(oriImg.shape[1]/2)
+    #         diff_y = (center_y - y)/(oriImg.shape[0]/2)
 
     cv2.imshow('demo', canvas)
     if cv2.waitKey(1) & 0xFF == ord('q'):
